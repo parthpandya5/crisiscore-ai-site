37ba4d33c57e5f9059096fe5e726d494ed7f82e8
parthpandya5 <pandyanparth@gmail.com>
2025-07-04 18:06:39 -0400

    feat(front-end): add feature cards section to home page

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -2,10 +2,13 @@
   <div>
     <Hero />
     <ImpactStats />
++   <FeatureCards />
+    <!-- upcoming: Partners, Testimonials -->
   </div>
 </template>
 
 <script setup>
-import Hero from '../components/Hero.vue';
-import ImpactStats from '../components/ImpactStats.vue';
+import Hero from '../components/Hero.vue'
+import ImpactStats from '../components/ImpactStats.vue'
+import FeatureCards from '../components/FeatureCards.vue'
 </script>
